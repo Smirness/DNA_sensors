9e24cab0c6a5cbaa0ba6f3167a2e502d57355f31
smirness <smirnovviik@gmail.com>
2023-01-07 12:24:27 +0300

    Adding NA_tools

---
+++ NA_tools.py
@@ -0,0 +1,69 @@
+def transcribe(sequence):
+
+    if check_seq(sequence) == True:
+        transcribe_dict = {
+            'A': 'A',
+            'a': 'a',
+            'T': 'U',
+            't': 'u',
+            'U': 'T',
+            'u': 'T',
+            'C': 'C',
+            'c': 'c',
+            'G': 'G',
+            'g': 'g',
+        }
+
+        result = ''
+        for nt in sequence:
+            result += transcribe_dict[nt]
+
+        return result
+
+def complement(sequence):
+
+    if check_seq(sequence) == True:
+        complement_dict = {
+            'A': 'T',
+            'a': 't',
+            'T': 'A',
+            't': 'a',
+            'C': 'G',
+            'c': 'g',
+            'G': 'C',
+            'g': 'c',
+        }
+        result =''
+        for nt in sequence:
+            result += complement_dict[nt]
+        return result
+
+def reverse(sequence):
+    if check_seq(sequence) == True:
+        return sequence[::-1]
+
+def reverse_complement(sequence):
+    if check_seq(sequence) == True:
+        result = ''
+        for nt in sequence:
+            result += complement[nt]
+        return result[::-1]
+
+def check_seq(sequence):
+    check_seq = True
+
+    if check_seq is True:
+        for nt in sequence:
+
+            if check_seq is False:
+                break
+
+            if nt in 'AaTtUuCcGg':
+                check_seq = True
+            else:
+                check_seq = False
+
+    return check_seq
+
+sequence = 'ATCG'
+print(transcribe(sequence))
